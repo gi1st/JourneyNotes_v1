c03e705ff7af43e2d2ada0d82d5e8a6b2070bcd3
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-07-11 10:59:19 +0200

    Create journey_detail_view in journeys/views.py

--- journeys/views.py
+++ journeys/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 
-from journeys.models import Route
+from journeys.models import Route, Comment
 
 
 def journey_view(request: HttpRequest) -> HttpResponse:
@@ -15,8 +15,17 @@ def journey_view(request: HttpRequest) -> HttpResponse:
 
 
 
-def journey_detail_view(request: HttpRequest) -> HttpResponse:
-    pass
+def journey_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
+    journey_detail = (
+        Route.objects
+        .select_related("author")
+        .annotate(comment_count=Count("comments"))
+        .prefetch_related(
+            Prefetch("comments", queryset=Comment.objects.select_related("author"))
+        )
+        .get(pk=pk)
+    )
+    return render(request, "journeys/journey_detail.html", {"journey_detail": journey_detail})
 
 
 def journey_create_view(request: HttpRequest) -> HttpResponse:
