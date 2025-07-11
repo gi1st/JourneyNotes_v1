7f12418e99d49e7afc177951c45c333efc8691a7
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-07-11 15:29:05 +0200

    Rename JourneyCreateForm to JourneyForm

--- journeys/forms.py
+++ journeys/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from journeys.models import Route
 
-class JourneyCreateForm(forms.ModelForm):
+class JourneyForm(forms.ModelForm):
     class Meta:
         model = Route
         fields = ["title", "content"]
