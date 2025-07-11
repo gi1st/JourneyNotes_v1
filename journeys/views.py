aa95242e91bcd89679389f2750cab45f22a9de3b
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-07-11 09:41:46 +0200

    Create journey_view in journeys/views.py; add stubs for other journey views

--- journeys/views.py
+++ journeys/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.db.models import Count
 
-# Create your views here.
+from journeys.models import Route
+
+
+def journey_view(request: HttpRequest) -> HttpResponse:
+    journeys_list = (
+        Route.objects
+        .select_related("author")
+        .annotate(comment_count=Count("comments"))
+    )
+    return render(request, "journeys/journeys.html", {"journeys_list": journeys_list})
+
+
+
+def journey_detail_view(request: HttpRequest) -> HttpResponse:
+    pass
+
+
+def journey_create_view(request: HttpRequest) -> HttpResponse:
+    pass
+
+
+def journey_delete_view(request: HttpRequest) -> HttpResponse:
+    pass
