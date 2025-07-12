151f7ba5fc0ac9a15cd3f542ac3adf2049b278b8
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-07-12 12:47:35 +0200

    Create view register_view and basic template register.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+from accounts.forms import RegisterForm
 
 
 def registrer_view(request):
-    pass
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("journeys:journeys_list")
+    else:
+        form = RegisterForm()
+    return render(request, "accounts/register.html", {"form": form})
 
 
 def login_view(request):
