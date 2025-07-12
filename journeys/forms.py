4a4498f4c64d6199f42f81080f05161a62c2de2c
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-07-12 18:30:35 +0200

    Add comments functionality: form, URL, views, and detail template updates

--- journeys/forms.py
+++ journeys/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
-from journeys.models import Route
+from journeys.models import Route, Comment
 
 class JourneyForm(forms.ModelForm):
     class Meta:
         model = Route
         fields = ["title", "content"]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
--- journeys/urls.py
+++ journeys/urls.py
@@ -6,6 +6,7 @@ from journeys.views import(
     journey_create_view,
     journey_update_view,
     journey_delete_view,
+    add_comment_view,
 )
 
 urlpatterns = [
@@ -13,7 +14,8 @@ urlpatterns = [
     path("journeys/<int:pk>/", journey_detail_view, name="journey_detail"),
     path("journeys/create/", journey_create_view, name="journey_create"),
     path("journeys/<int:pk>/update/", journey_update_view, name="journey_update"),
-    path("journeys/<int:pk>/delete/", journey_delete_view, name="journey_delete")
+    path("journeys/<int:pk>/delete/", journey_delete_view, name="journey_delete"),
+    path("journeys/<int:pk>/add-comment", add_comment_view, name="add_comment"),
 ]
 
 app_name = "journeys"
