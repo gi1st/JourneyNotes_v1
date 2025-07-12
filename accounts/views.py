e92d30c65aeee5f402e80a77b43096ac00ee88c4
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-07-12 13:22:46 +0200

    Create view login_view and basic template login.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 
 from accounts.forms import RegisterForm
 
@@ -17,7 +18,15 @@ def registrer_view(request):
 
 
 def login_view(request):
-    pass
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("journeys:journeys_list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
 
 
 def logout_view(request):
